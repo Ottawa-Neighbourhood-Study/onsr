3c7ff5c623c38e87aa7364a94c68f210d6930a80
Christopher Belanger <christopher.a.belanger@gmail.com>
2020-12-06 15:34:54 -0500

    Added ons_shp as included data. Updated get_pts_neighbourhood() to work with included data.

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Details and geographies for Ottawa Neighbourhood Study (ONS) Gen2 boundaries.
+#'
+#' A dataset containing the ONS IDs, English and French names, and boundaries
+#' for 111 neighbourhoods in Ottawa.
+#'
+#'
+#' @format A simple feature geometry collection with 111 features/rows
+#' and 3 fields/columns:
+#' \describe{
+#'   \item{ONS_ID}{Unique identifier, integer}
+#'   \item{Name}{English name}
+#'   \item{Name_FR}{French name}
+#'   \item{geometry}{Neighbourhood boundary, multipolygon geometry}
+#'   ...
+#' }
+#' @source \url{https://www.neighbourhoodstudy.ca/}
+"ons_shp"
+
+
+Name <- Name_FR<-  ONS_ID <- NULL
--- R/get_pts_neighbourhood.R
+++ R/get_pts_neighbourhood.R
@@ -10,10 +10,10 @@
 #' @return A simple feature collection of class sf with all columns in pts plus additional columns for each point's corresponding polygon (if applicable).
 #' @export
 #'
-get_pts_neighbourhood <- function(pts, pgon = ons_shp){
+get_pts_neighbourhood <- function(pts, pgon = onsr::ons_shp){
   # make sure user gives inputs
   if (missing(pts)) stop ('Argument "pts" is missing. Which points would you like to locate?')
-  if (missing(pgon)) stop ('Argument "pgon" is missing. Consider running: ons_shp <- get_ons_shp()')
+ # if (missing(pgon)) stop ('Argument "pgon" is missing. Consider running: ons_shp <- get_ons_shp()')
 
   # check input validity
   if (!"sf" %in% class(pts)) stop("Invalid input: pts must have class 'sf', e.g. a shapefile loaded with sf::read_sf().")

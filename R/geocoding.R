987075859605a087fe782737f0f47f8ade7cbf7e
Christopher Belanger <christopher.a.belanger@gmail.com>
2020-12-08 16:35:17 -0500

    Added new function: geocode_ottawa() Renamed old functions to match: geocode_gmap(), geocode_gmap_one()

--- R/geocoding.R
+++ R/geocoding.R
@@ -8,7 +8,7 @@
 #'
 #' @return The original dataframe with geocoded latitude and longitude in new columns lat and lng. Addresses that are invalid or not found return NA.
 #' @export
-gmap_geocode <- function(data, var, api_key = NA, verbose = FALSE, wait_nicely = TRUE) {
+geocode_gmap <- function(data, var, api_key = NA, verbose = FALSE, wait_nicely = TRUE) {
   # TODO: add default column for addresses.
 
   # get the addresses in a character vector
@@ -78,7 +78,7 @@ gmap_geocode <- function(data, var, api_key = NA, verbose = FALSE, wait_nicely =
 #'
 #' @return A dataframe with geocoded latitute and logitude.
 #' @export
-gmap_geocode_one <- function(address = "1243 Willowdale Ave, Ottawa, ON", api_key = NA, verbose = FALSE, wait_nicely = TRUE) {
+geocode_gmap_one <- function(address = "1243 Willowdale Ave, Ottawa, ON", api_key = NA, verbose = FALSE, wait_nicely = TRUE) {
   # must give an api key
   if (base::is.na(api_key)) stop ("Please provide a valid Google Cloud API key. Get one for free here: https://cloud.google.com/free/")
 
@@ -122,3 +122,84 @@ gmap_geocode_one <- function(address = "1243 Willowdale Ave, Ottawa, ON", api_ke
 
   return (dplyr::tibble (lat=lat, lng=lng))
 }
+
+
+
+#' Tidy Geocoding with the City of Ottawa's API
+#'
+#' @param data A dataframe containing a column with addresses stored as characters.
+#' @param var A column with addresses stored as characters.
+#' @param verbose Boolean flag controlling console updates. Defaults to FALSE.
+#' @param wait_nicely Boolean flag controlling API rate. Defaults to TRUE.
+#'
+#' @return The original dataframe with geocoded latitude and longitude in new columns lat and lng. Addresses that are invalid or not found return NA.
+#' @export
+geocode_ottawa <- function(data, var, verbose = FALSE, wait_nicely = TRUE) {
+  # TODO: add default column for addresses.
+  # TODO: do batching for >1000 addresses.
+
+  # get the addresses in a one-column tibble
+  addresses <- data %>% dplyr::select ({{var}})
+  num_addresses <- base::length(addresses)
+
+  # create json in the right format for the api
+  # we create a temp column so we can nest, then we remove it
+  # then we use purrr::map to make lists of the right kind (couldn't get nested tibbles to work)
+  # convert to json, then trim the first and last character
+  json_addresses <- addresses %>%
+    dplyr::mutate(temp_for_json = 1:nrow(addresses)) %>%
+    dplyr::mutate( attributes =
+              purrr::map2(temp_for_json,
+                          address,
+                          function(x,y) list(OBJECTID = jsonlite::unbox(x),
+                                             SingleLine = jsonlite::unbox(y)))) %>%
+    dplyr::select(attributes) %>%
+    tidyr::nest(records = attributes) %>%
+    jsonlite::toJSON() %>%
+    base::as.character()
+
+  json_addresses <- json_addresses %>%
+    base::substr(2, (nchar(json_addresses)-1))
+
+  base_url <- "https://maps.ottawa.ca/arcgis/rest/services/compositeLocator/GeocodeServer/geocodeAddresses"
+
+  # set up a url object using the base url
+  url <- httr::parse_url(base_url)
+
+  # tell the url object we'd like to query that object
+  url$query <- base::list("addresses" = json_addresses,
+                          "f" = "json",
+                          "outSR" = "4326")
+
+  # make a url out of all this information
+  url_full <- httr::build_url(url)
+
+  # now try to get it
+  q <- httr::POST(url=url_full)
+
+  # if we got a good response code
+  # extract the geocoded location
+  if (q$status_code == 200){
+
+    latlon <- httr::content(q) %>%
+      jsonlite::fromJSON()
+
+    lng <- latlon$locations$location$x
+    lat <- latlon$locations$location$y
+  }
+
+  # combine our results back with the original input data
+  # convert NaNs to NAs to be consistent with Google Maps' responses
+  results <- dplyr::bind_cols(
+    data,
+    dplyr::tibble(lat = lat, lng = lng) %>%
+      dplyr::mutate(lat = dplyr::if_else(base::is.nan(lat), NA_real_, lat),
+                     lng= dplyr::if_else(base::is.nan(lng), NA_real_, lng))
+  )
+
+  return(results)
+}
+
+
+# add these variables as NULL so R CMD CHECK won't throw notes about them
+latlon <- address <- temp_for_json <- NULL

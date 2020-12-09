dbc806f841091ba2ae68ba4667270ac09541eb2c
Christopher Belanger <christopher.a.belanger@gmail.com>
2020-12-09 15:02:14 -0500

    geocode_ottawa() now uses POST, can accept many more addresses

--- R/geocoding.R
+++ R/geocoding.R
@@ -127,20 +127,36 @@ geocode_gmap_one <- function(address = "1243 Willowdale Ave, Ottawa, ON", api_ke
 
 #' Tidy Geocoding with the City of Ottawa's API
 #'
-#' @param data A dataframe containing a column with addresses stored as characters.
+#' @param data A dataframe containing a column with addresses stored as
+#'   characters.
 #' @param var A column with addresses stored as characters.
 #' @param verbose Boolean flag controlling console updates. Defaults to FALSE.
 #' @param wait_nicely Boolean flag controlling API rate. Defaults to TRUE.
 #'
-#' @return The original dataframe with geocoded latitude and longitude in new columns lat and lng. Addresses that are invalid or not found return NA.
+#' @description Geocode a column of addresses using the City of Ottawa's
+#'   geocoding service. No API key is needed, and the service uses ESRI's
+#'   technology under the hood. Consult
+#'   \href{https://open.ottawa.ca/pages/developer-resources}{the City of
+#'   Ottawa's developer resources} or
+#'   \href{https://developers.arcgis.com/rest/services-reference/geocode-addresses.htm}{ESRI's
+#'   API documentation} for more information.
+#'
+#' @return The original dataframe with geocoded latitude and longitude in new
+#'   columns lat and lng. Addresses that are invalid or not found return NA.
 #' @export
 geocode_ottawa <- function(data, var, verbose = FALSE, wait_nicely = TRUE) {
   # TODO: add default column for addresses.
   # TODO: do batching for >1000 addresses.
 
   # get the addresses in a one-column tibble
-  addresses <- data %>% dplyr::select ({{var}})
-  num_addresses <- base::length(addresses)
+  addresses <- data %>% dplyr::select (address = {{var}})
+
+  # ESRI doesn't like prefixed apartment numbers or units, so we remove them
+  # we also remove anything after a comma: we only want the street name
+  # we're only searching within Ottawa
+  addresses <- addresses %>%
+    dplyr::mutate(address = base::gsub(x = address, pattern = "^\\d*\\w*-", replacement = ""),
+                  address = base::gsub(x = address, pattern = ",.*", replacement = ""))
 
   # create json in the right format for the api
   # we create a temp column so we can nest, then we remove it
@@ -149,10 +165,10 @@ geocode_ottawa <- function(data, var, verbose = FALSE, wait_nicely = TRUE) {
   json_addresses <- addresses %>%
     dplyr::mutate(temp_for_json = 1:nrow(addresses)) %>%
     dplyr::mutate( attributes =
-              purrr::map2(temp_for_json,
-                          address,
-                          function(x,y) list(OBJECTID = jsonlite::unbox(x),
-                                             SingleLine = jsonlite::unbox(y)))) %>%
+                     purrr::map2(temp_for_json,
+                                 address,
+                                 function(x,y) list(OBJECTID = jsonlite::unbox(x),
+                                                    SingleLine = jsonlite::unbox(y)))) %>%
     dplyr::select(attributes) %>%
     tidyr::nest(records = attributes) %>%
     jsonlite::toJSON() %>%
@@ -166,16 +182,14 @@ geocode_ottawa <- function(data, var, verbose = FALSE, wait_nicely = TRUE) {
   # set up a url object using the base url
   url <- httr::parse_url(base_url)
 
-  # tell the url object we'd like to query that object
-  url$query <- base::list("addresses" = json_addresses,
-                          "f" = "json",
-                          "outSR" = "4326")
-
   # make a url out of all this information
   url_full <- httr::build_url(url)
 
   # now try to get it
-  q <- httr::POST(url=url_full)
+  q <- httr::POST(url=url_full,
+                  body =  base::list("addresses" = json_addresses,
+                                     "f" = "json",
+                                     "outSR" = "4326"))
 
   # if we got a good response code
   # extract the geocoded location
@@ -194,12 +208,14 @@ geocode_ottawa <- function(data, var, verbose = FALSE, wait_nicely = TRUE) {
     data,
     dplyr::tibble(lat = lat, lng = lng) %>%
       dplyr::mutate(lat = dplyr::if_else(base::is.nan(lat), NA_real_, lat),
-                     lng= dplyr::if_else(base::is.nan(lng), NA_real_, lng))
+                    lng= dplyr::if_else(base::is.nan(lng), NA_real_, lng))
   )
 
   return(results)
 }
 
 
+
 # add these variables as NULL so R CMD CHECK won't throw notes about them
 latlon <- address <- temp_for_json <- NULL
+
